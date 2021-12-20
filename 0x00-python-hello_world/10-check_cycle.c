a79d9f17380b6e66ab5a30c83318b641e14957dc
MarionBaussart <marion.baussart@gmail.com>
2021-12-20 11:43:26 +0000

    mandatory tasks added

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: singly list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+    listint_t *start = list, *search = list;
+
+    while (search && search->next)
+    {
+        while (start != search && start != search->next)
+            start = start->next;
+        if (start == search->next)
+			return (1);
+        search = search->next;
+        start = list;
+    }
+    return (0);
+}
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print("{0}{0}{0}\n{0:.9}".format(str))
+# we can use also
+# print(str * 3) and then print(str[:9])
