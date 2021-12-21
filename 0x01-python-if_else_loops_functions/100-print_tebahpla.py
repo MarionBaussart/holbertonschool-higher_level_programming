dae3de510d4ce28b20ca14922eeedded8b4dca87
MarionBaussart <marion.baussart@gmail.com>
2021-12-21 15:13:06 +0000

    T13

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in reversed(range(ord('a'), ord('z') + 1)):
-    print("{:c}".format(alphabet if alphabet % 2 == 0 else alphabet - 32), end="")
+for letter in reversed(range(ord('a'), ord('z') + 1)):
+    print("{:c}".format(letter if letter % 2 == 0 else letter - 32), end="")
