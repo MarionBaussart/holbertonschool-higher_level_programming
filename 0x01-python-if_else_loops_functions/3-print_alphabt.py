9bec438d47364f530d39b1148f52005eb4a4fa3d
MarionBaussart <marion.baussart@gmail.com>
2021-12-21 12:53:10 +0000

    pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
-	if chr(alphabet) != 'e' and chr(alphabet) != 'q':
-		print("{:c}".format(alphabet), end="")
+    if chr(alphabet) != 'e' and chr(alphabet) != 'q':
+        print("{:c}".format(alphabet), end="")
