aeaa8977cfb40696055f98babb588cd3f4eb8015
MarionBaussart <marion.baussart@gmail.com>
2021-12-23 11:52:05 +0000

    T2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        for nb_arg in range(1, len(sys.argv)):
+            print("{}: {}".format(nb_arg, sys.argv[nb_arg]))
