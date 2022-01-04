2915a529b3bee64f0f0418d4673a686f0feed011
MarionBaussart <marion.baussart@gmail.com>
2022-01-04 14:16:36 +0000

    T9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that finds the biggest integer of a list
+"""
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    else:
+        integer_max = 0
+        for integer in my_list:
+            if integer > integer_max:
+                integer_max = integer
+        return (integer_max)
+ 
