3426136c4ccce93aeedfecffc5ad504df146650d
MarionBaussart <marion.baussart@gmail.com>
2022-01-04 12:37:40 +0000

    T3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,7 @@ function that replaces an element of a list at a specific position
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for integer in my_list:
-        print("{}".format(integer))
+    if my_list:
+        my_list.reverse()
+        for integer in my_list:
+            print("{}".format(integer))
