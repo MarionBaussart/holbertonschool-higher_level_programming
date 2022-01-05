a94846def0952c68b83852e55eae59102af0bcfe
MarionBaussart <marion.baussart@gmail.com>
2022-01-05 22:22:39 +0000

    T4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that returns a set of all elements present in only one set
+"""
+
+
+def only_diff_elements(set_1, set_2):
+    different_list = set_1 ^ set_2
+    return (different_list)
