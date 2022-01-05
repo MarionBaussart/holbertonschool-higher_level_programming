dee08ebb6ad582375c38ba54146a9f4eeb000c60
MarionBaussart <marion.baussart@gmail.com>
2022-01-05 21:52:48 +0000

    T1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that replaces all occurrences of an element by another in a new list
+"""
+
+
+def search_replace(my_list, search, replace):
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
