c8ff3572a20119d487d3578fff4419fe4853c16f
MarionBaussart <marion.baussart@gmail.com>
2022-01-17 16:10:16 +0000

    T1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,8 +7,7 @@ function that prints an integer with "{:d}".format().
 def safe_print_integer(value):
 
     try:
-        if isinstance(value, int):
-            print("{}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except:
         return False
