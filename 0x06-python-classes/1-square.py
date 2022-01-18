dd4b591c88c8ddd8afd929699373c9309bae8af4
MarionBaussart <marion.baussart@gmail.com>
2022-01-18 15:03:05 +0000

    T1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
+class Square:
+    """ class Square that defines a square """
+
+    def __init__(self, size):
+        """
+        Initializes the size of the square
+        @size: private instance attribute
+        """
+        self.__size = size
