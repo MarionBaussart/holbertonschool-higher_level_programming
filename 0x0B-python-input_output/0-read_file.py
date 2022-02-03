5acce8a5eb1cc4b1e62844acd8d1a54069e36e86
MarionBaussart <marion.baussart@gmail.com>
2022-02-03 15:02:04 +0000

    T0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module contain function read_file
+"""
+
+
+def read_file(filename=""):
+    """ function that reads a text file (UTF8) and prints it to stdout
+        Args:
+            filename: name of the file
+        Return:
+            no return
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
