22ecf2792a8353bfc1c98df210f83a5afa365b15
MarionBaussart <marion.baussart@gmail.com>
2022-03-16 20:41:04 +0000

    T8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,5 +5,5 @@
 -- Results must be sorted in ascending order by cities.id
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE states.name = California AND states.state_id = cities.id
+WHERE states.name = California AND states.id = cities.state_id
 ORDER BY cities.id ASC;
