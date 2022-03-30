5ab3cbabde7b6bcc0f6eeff25fdb9afd5804e0cd
MarionBaussart <marion.baussart@gmail.com>
2022-03-30 15:22:18 +0000

    T8, update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,3 +117,22 @@ class Rectangle(Base):
         """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """
+        Public instance method : assigns an argument to each attribute
+        Argument order is super important
+        Args:
+            self: first argument to instance methods
+            *args: list of arguments - no-keyworded arguments
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
