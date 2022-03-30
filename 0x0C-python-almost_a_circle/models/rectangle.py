04ef651bc77e6db30b79bd7061904d32ff8f0d9a
MarionBaussart <marion.baussart@gmail.com>
2022-03-30 15:28:27 +0000

    T9, update update, kwrgs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,21 +118,26 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        Public instance method : assigns an argument to each attribute
-        Argument order is super important
+        Public instance method : assigns a key/value argument to attributes
         Args:
             self: first argument to instance methods
             *args: list of arguments - no-keyworded arguments
+            **kwargs: double pointer to a dictionary: key/value
+            (keyworded arguments)
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+        if args is not None and len(args) > 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
