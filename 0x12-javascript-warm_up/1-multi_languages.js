98e58ae50e64d3def952bc74e622f9f80d0ed7c2
MarionBaussart <marion.baussart@gmail.com>
2022-04-04 10:08:23 +0000

    T1, print multiple lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+script that prints “JavaScript is amazing”
+*/
+
+const firstLine = 'C is fun'; // create a constant var
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
