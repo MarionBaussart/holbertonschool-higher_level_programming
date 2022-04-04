085b5da3f6edf4d6157d5495cbd67fb46c20b019
MarionBaussart <marion.baussart@gmail.com>
2022-04-04 12:36:57 +0000

    T7, print x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+script that prints x times “C is fun”
+*/
+let occurence = parseInt(process.argv[2]);
+let i = 0;
+
+if (isNaN(occurence)) {
+  console.log('Missing number of occurrences');
+}
+else {
+  while (i < parseInt(process.argv[2])) {
+    console.log('C is fun');
+    i++;
+  }
+}
