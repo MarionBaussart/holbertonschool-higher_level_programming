4b89702590f6662053fe5bf8ae09d42d6f2251cc
MarionBaussart <marion.baussart@gmail.com>
2022-04-04 12:03:44 +0000

    T3, semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,7 @@
 script that prints prints the first argument passed to it
 */
 if (!process.argv[2]) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
-  
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
