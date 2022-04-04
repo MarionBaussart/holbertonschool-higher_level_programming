5881532dea648763903b695836a6b4ec255c2b3d
MarionBaussart <marion.baussart@gmail.com>
2022-04-04 12:59:56 +0000

    T9, addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+script that prints the addition of 2 integers
+*/
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(firstInt, secondInt));
