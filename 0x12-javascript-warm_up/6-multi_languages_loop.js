a59b28bb9b8d4ac43d052d8338ce509aeb9d06d8
MarionBaussart <marion.baussart@gmail.com>
2022-04-04 12:29:02 +0000

    T6, loop while

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+script that prints 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop
+*/
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < lines.length) {
+  console.log(lines[i]);
+  i++;
+}
