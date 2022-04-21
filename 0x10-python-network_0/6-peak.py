b9d7d808bfe9dab4b05537fcf27b6b71fb4c6907
MarionBaussart <marion.baussart@gmail.com>
2022-04-21 13:54:31 +0000

    T6, peaks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,7 +18,9 @@ def find_peak(list_of_integers):
     if list_of_integers[middle] >= list_of_integers[middle - 1] and\
        list_of_integers[middle] >= list_of_integers[middle + 1]:
         return list_of_integers[middle]
-    if list_of_integers[middle - 1] and list_of_integers[middle] < list_of_integers[middle - 1]:
+    if list_of_integers[middle - 1] and\
+       list_of_integers[middle] < list_of_integers[middle - 1]:
         return find_peak(list_of_integers[:middle])
-    if list_of_integers[middle + 1] and list_of_integers[middle] < list_of_integers[middle + 1]:
+    if list_of_integers[middle + 1] and\
+       list_of_integers[middle] < list_of_integers[middle + 1]:
         return find_peak(list_of_integers[middle:])
