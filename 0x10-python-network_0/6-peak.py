7bcce8a075d43420f246026aed182e5507d102c6
MarionBaussart <marion.baussart@gmail.com>
2022-04-21 13:22:32 +0000

    T6, peaks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" function that finds a peak in a list of unsorted integers """
+
+def find_peak(list_of_integers):
+
+    if list_of_integers == []:
+        return None
+
+    
+    length = len(list_of_integers)
+    middle = int(length / 2)
+
+    if length == 1:
+        return list_of_integers[0]
+    if length == 2:
+        return max(list_of_integers)
+    if list_of_integers[middle] >= list_of_integers[middle - 1] and\
+        list_of_integers[middle] >= list_of_integers[middle + 1]:
+        return list_of_integers[middle]
+    if list_of_integers[middle] < list_of_integers[middle - 1]:
+        return find_peak(list_of_integers[:middle])
+    if list_of_integers[middle] < list_of_integers[middle + 1]:
+        return find_peak(list_of_integers[middle:])
+
+
+
+
+
+    
