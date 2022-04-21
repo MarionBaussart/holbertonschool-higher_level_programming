dd8ff5477a52b023f61fd50e5b11f8f097295db2
MarionBaussart <marion.baussart@gmail.com>
2022-04-21 11:48:33 +0000

    T0, size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
+#curl -s for silent mode
 
-curl -s "$1" | wc -c  #curl -s for silent mode
+curl -s "$1" | wc -c
