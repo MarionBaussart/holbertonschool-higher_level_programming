11a867a58ed882adb3ce03ee73d0836ae0be0021
MarionBaussart <marion.baussart@gmail.com>
2022-04-22 10:01:04 +0000

    T0, status of fetched page

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status
+    and give its status """
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(html)))
+        print("    - content: {}".format(html))
+        print("    - utf8 content: {}".format(html.decode('utf-8')))
