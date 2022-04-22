f93f9cbe09faa9577534a83aa3238baac8a5362b
MarionBaussart <marion.baussart@gmail.com>
2022-04-22 14:02:55 +0000

    T4, requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status
+    and give its status """
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
