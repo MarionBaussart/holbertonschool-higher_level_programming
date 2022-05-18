2223decf7e994124c4fbf00f8f8ce9401bf26f32
MarionBaussart <marion.baussart@gmail.com>
2022-05-18 12:42:24 +0000

    add tasks

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/*
+Script that display the status code of a GET request
+*/
+
+const axios = require('axios').default;
+
+axios({
+  method: 'get',
+  url: process.argv[2]
+})
+.then((res) => {
+  console.log('code: ' + res.status);
+})
+.catch((error) => {
+  console.log('code: ' + error.response.status);
+});
