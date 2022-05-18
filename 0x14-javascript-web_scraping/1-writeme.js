7624e9f3e6e483ff86dea11542b7ba378a2dee80
MarionBaussart <marion.baussart@gmail.com>
2022-05-18 12:45:02 +0000

    semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,10 @@
 Script that writes a string to a file
 */
 const fs = require('fs');
-const content = process.argv[3]
+const content = process.argv[3];
 
 fs.writeFile(process.argv[2], content, err => {
   if (err) {
     return console.log(err);
   }
-})
+});
